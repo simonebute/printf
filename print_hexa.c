30fbe22b34318a5b01369b498e21264d4ab173ef
simonebute <bedrockresources21@gmail.com>
2023-11-15 09:36:44 -0800

    task3

---
+++ print_hexa.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_hexa - prints an hexadecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_hexa(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = tem % 16;
+		tem /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
